941e0459f10b54d19c8571300277cf94065107ee
Pei-Lun Lee <ypcat6@gmail.com>
2015-10-01 13:05:09 +0800

    Refactor transition function with check/mutate

--- dopewars.py
+++ dopewars.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
-from copy import deepcopy
 from operator import add, mul, div
 import random
+
+locations = ['Bronx', 'Ghetto', 'Central Park', 'Manhattan', 'Coney Island', 'Brooklyn']
+
+drugs = ['Acid', 'Cocaine', 'Ludes', 'PCP', 'Heroin', 'Weed', 'Shrooms', 'Speed']
+
+price_range = {
+    'Acid': {'low': 1000, 'high': 4500},
+    'Cocaine': {'low': 15000, 'high': 30000},
+    'Ludes': {'low': 10, 'high': 60},
+    'PCP': {'low': 1000, 'high': 3500},
+    'Heroin': {'low': 5000, 'high': 14000},
+    'Weed': {'low': 300, 'high': 900},
+    'Shrooms': {'low': 600, 'high': 1350},
+    'Speed': {'low': 70, 'high': 250}
+}
 
 events = [
     {'freq': 13, 'op': mul, 'amount': 4, 'drug': 'Weed', 'message': "The cops just did a big Weed bust!  Prices are sky-high!"},
@@ -24,20 +38,7 @@ events = [
     {'freq': 35, 'op': add, 'amount': 4, 'drug': 'PCP', 'message': "You found some PCP on a dead dude in the subway!"}
 ]
 
-drugs = ['Acid', 'Cocaine', 'Ludes', 'PCP', 'Heroin', 'Weed', 'Shrooms', 'Speed']
-locations = ['Bronx', 'Ghetto', 'Central Park', 'Manhattan', 'Coney Island', 'Brooklyn']
-price_range = {
-    'Acid': {'low': 1000, 'high': 4500},
-    'Cocaine': {'low': 15000, 'high': 30000},
-    'Ludes': {'low': 10, 'high': 60},
-    'PCP': {'low': 1000, 'high': 3500},
-    'Heroin': {'low': 5000, 'high': 14000},
-    'Weed': {'low': 300, 'high': 900},
-    'Shrooms': {'low': 600, 'high': 1350},
-    'Speed': {'low': 70, 'high': 250}
-}
-
-sessions = {}
+games = {}
 
 def randint():
     return random.randrange(0, 2 << 31)
@@ -45,50 +46,100 @@ def randint():
 def dice(n):
     return randint() % n == 0
 
-def public(func):
-    def new_func(*args, **kw):
-        game = func(*args, **kw)
-        game['messages'] += get_messages(game)
-        return deepcopy(game)
-    return new_func
-
 def get_prices(leaveout=3):
     keys = random.sample(drugs, len(drugs) - random.randint(0, leaveout))
     return {k: random.randint(price_range[k]['low'], price_range[k]['high']) for k in keys}
 
-def get_messages(game):
-    messages = []
-    state = game['state']
-    if state == 'jet':
-        messages = ['Where to, dude?']
-    elif state == 'buy_sell':
-        for drug in drugs:
-            count = game['drugs'].get(drug, 0)
-            price = game['prices'].get(drug)
-            dollar = '$' if price else ''
-            messages.append("%s amount: %d price: %s%s" % (drug, count, dollar, str(price)))
-        messages.append("Cash $%d" % game['cash'])
-        messages.append("Debt $%d" % game['debt'])
-        messages.append("Savings $%d" % game['bank'])
-        messages.append("Coat %d/%d" % (get_total(game), game['coat']))
-    elif state == 'buy_select':
-        drug = game['select']
-        price = game['prices'][drug]
-        amount = game['cash'] / price
-        amount = amount if amount < 1000 else 'a lot'
-        messages = ["Buy %s at %d each, you can afford %s" % (drug, price, amount)]
-    return messages
-
 def get_total(game):
     return sum(game['drugs'].values())
 
-@public
+def select(items, input):
+    try:
+        return items[int(input) - 1]
+    except:
+        return None
+
+def selector(items):
+    return "1-%d" % len(items)
+
+#action
+def quit(game, input):
+    if input == 'quit' or input == 'q':
+        game['finish'] = True
+        return True
+
+#action
+def buy(game, input):
+    if input == 'buy':
+        pass
+#    if state == 'buy_select':
+#        drug = game['select']
+#        price = game['prices'][drug]
+#        amount = game['cash'] / price
+#        amount = amount if amount < 1000 else 'a lot'
+#        messages = ["Buy %s at %d each, you can afford %s" % (drug, price, amount)]
+#    return messages
+
+#action
+def sell(game, input):
+    pass
+
+def format_price(game, drug):
+    amount = game['drugs'].get(drug, 0)
+    price = game['prices'].get(drug)
+    price = ("$%d" % price) if price else 'None'
+    return "%s price: %s you have: %d" % (drug, price, amount)
+
+def trade_messages(game):
+    return [format_price(game, drug) for drug in drugs] + [
+        "Cash $%d" % game['cash'],
+        "Debt $%d" % game['debt'],
+        "Savings $%d" % game['bank'],
+        "Coat %d/%d" % (get_total(game), game['coat']),
+        "Time remaining: %d day%s" %(game['days'], 's' if game['days'] > 1 else ''),
+        "You are at %s" % game['location']
+    ]
+
+def goto_trade(game):
+    game['options'] = [buy, sell, jet, quit]
+    game['messages'] = trade_messages(game) + ['buy sell jet quit'] # XXX +=
+
+#action
+def jet2(game, input):
+    location = select(locations, input)
+    if not location:
+        return
+    assert location != game['location'], 'same location'
+    game['debt'] += game['debt'] >> 3 if game['days'] < 31 else 0
+    game['bank'] += game['bank'] >> 4
+    game['days'] -= 1
+    game['prices'] = get_prices()
+    game['location'] = location
+    goto_trade(game)
+    return True
+
+#action
+def cancel(game, input):
+    if input == 'cancel':
+        goto_trade(game)
+        return True
+
+def jet_messages(game):
+    return ['Where to, dude?'] + ["%d %s" % pair for pair in enumerate(locations, 1)]
+
+#action
+def jet(game, input):
+    if input == 'jet':
+        game['options'] = [jet2, cancel, quit]
+        game['messages'] = jet_messages(game) + ["%s cancel quit" % selector(locations)]
+        return True
+
 def start(name):
-    session = randint()
-    while session in sessions:
-        session = randint()
-    sessions[session] = {
-        'session': session,
+    id = randint()
+    while id in games:
+        id = randint()
+    games[id] = {
+        'id': id,
         'name': name,
         'drugs': {},
         'cash': 2000,
@@ -99,141 +150,99 @@ def start(name):
         'location': '',
         'days': 31,
         'cops': 3,
-        'prices': get_prices(),
-        'select': '',
-        'messages': [],
-        'state': 'jet',
-        'options': locations,
+        'prices': {},
+        'drug': '',
+        'options': [jet2, quit],
+        'messages': jet_messages({}) + ["%s quit" % selector(locations)],
         'finish': False
     }
-    return sessions[session]
+    return id
 
-def validate(arg, options):
-    try:
-        if isinstance(options, dict):
-            return options['type'](arg)
-        assert arg in options
-        return arg
-    except:
-        assert 0, 'invalid input'
+def transition(id, input):
+    assert id in games, 'invalid id'
+    game = games[id]
+    for option in game['options'][:]:
+        if option(game, input):
+            return
+    assert 0, 'invalid input'
 
-@public
-def transition(session, arg):
-    assert session in sessions, 'invalid session'
-    game = sessions[session]
-    state = game['state']
-    game['messages'] = []
-    print 'state', state, 'arg', arg, 'options', game['options']
-    arg = validate(arg, game['options'])
-    print 'arg', arg
-    if arg == 'jet':
-        game.update(state='jet', options=locations+['cancel'])
-        return game
-    elif arg == 'cancel':
-        game['state'] = 'buy_sell'
-        return game
-    elif state == 'jet':
-        return jet(game, arg)
-    elif arg == 'buy':
-        game.update(state='buy', options=drugs)
-        return game
-    elif arg == 'sell':
-        game.update(state='sell', options=drugs)
-        return game
-    elif state == 'buy':
-        game.update(state='buy_select', select=arg, options={'type': int})
-        return game
-    elif state == 'sell':
-        game.update(state='sell_select', select=arg, options={'type': int})
-        return game
-    elif state == 'buy_select':
-        return buy(game, game['select'], arg)
-    elif state == 'sell_select':
-        return sell(game, game['select'], arg)
-    assert 0, 'invalid state'
+#def jet(game, location):
+#    try:
+#        location = locations[int(location)]
+#    except:
+#        pass
+#    if location == game['location'] or location not in locations:
+#        return
+#    game = advance(game)
+#    game['state'] = 'trade'
+#    #dice_fuzz(state)
+#    return game
 
-def jet(game, location):
-    print 'jet', 'location', location
-    assert location in locations and location != game['location'], 'invalid location'
-    game['debt'] += game['debt'] >> 3 if game['days'] < 31 else 0
-    game['bank'] += game['bank'] >> 4
-    game['days'] -= 1
-    game['prices'] = get_prices()
-    game['messages'] = []
-    #dice_fuzz(state)
-    game['state'] = 'buy_sell'
-    game['options'] = ['buy', 'sell', 'jet']
-    return game
+#@public
+#def fuzz_event(game):
+#    if dice(7) and game['days'] > 0 and get_total(game) > 0:
+#        return dict(game, options=['fight', 'escape'])
+#    return finish_event(game)
 
-def buy(game, drug, Amount):
-    raise NotImplementedError
+#@public
+#def finish_event(game):
+#    if game['days'] == 0:
+#        pass # XXX finish
+#    return coat_event(game)
 
-def sell(game, drug, Amount):
-    raise NotImplementedError
+#@public
+#def coat_event(game):
+#    if dice(10):
+#        return dict(game, options=['buycoat', 'notbuycoat'])
+#    return gun_event(game)
 
-@public
-def fuzz_event(game):
-    if dice(7) and game['days'] > 0 and get_total(game) > 0:
-        return dict(game, options=['fight', 'escape'])
-    return finish_event(game)
+#@public
+#def gun_event(game):
+#    if game['guns'] == 0 and dice(10):
+#        return dict(game, options=['buygun', 'notbuygun'])
+#    return random_events(game)
 
-@public
-def finish_event(game):
-    if game['days'] == 0:
-        pass # XXX finish
-    return coat_event(game)
+#@public
+#def random_events(game):
+#    for event in events:
+#        if dice(event['freq']) and event['drug'] in game['prices']:
+#            game['messages'].append(event['message'])
+#            price = event['op'](game['prices'][event['drug']], event['amount'])
+#            game['prices'][event['drug']] = price
+#    return loan_shark_event(game)
 
-@public
-def coat_event(game):
-    if dice(10):
-        return dict(game, options=['buycoat', 'notbuycoat'])
-    return gun_event(game)
+#@public
+#def loan_shark_event(game):
+#    if location == 'Bronx':
+#        return dict(game, options=['visit_loan_shark', 'not_visit_loan_shark'])
+#    return bank_event(game)
 
-@public
-def gun_event(game):
-    if game['guns'] == 0 and dice(10):
-        return dict(game, options=['buygun', 'notbuygun'])
-    return random_events(game)
+#@public
+#def bank_event(game):
+#    if location == 'Bronx':
+#        return dict(game, options=['visit_bank', 'not_visit_bank'])
+#    return dict(game, options=['buy', 'sell', 'jet'])
 
-@public
-def random_events(game):
-    for event in events:
-        if dice(event['freq']) and event['drug'] in game['prices']:
-            game['messages'].append(event['message'])
-            price = event['op'](game['prices'][event['drug']], event['amount'])
-            game['prices'][event['drug']] = price
-    return loan_shark_event(game)
+def is_finish(id):
+    # XXX garbage collection
+    assert id in games, 'invalid id'
+    game = games[id]
+    return game['finish']
 
-@public
-def loan_shark_event(game):
-    if location == 'Bronx':
-        return dict(game, options=['visit_loan_shark', 'not_visit_loan_shark'])
-    return bank_event(game)
-
-@public
-def bank_event(game):
-    if location == 'Bronx':
-        return dict(game, options=['visit_bank', 'not_visit_bank'])
-    return dict(game, options=['buy', 'sell', 'jet'])
+def get_message(id):
+    assert id in games, 'invalid id'
+    game = games[id]
+    return '\n'.join(game['messages'])
 
 def main():
     name = raw_input('Enter your name: ')
-    game = start(name)
-    while not game['finish']:
+    id = start(name)
+    while not is_finish(id):
         try:
-            print '\n'.join(game['messages'])
-            options = game['options']
-            if isinstance(options, list):
-                print 'Options:'
-                for i, option in enumerate(game['options']):
-                    print i, option
-            print 'q Quit'
-            arg = raw_input('? ')
-            if arg == 'q':
-                break
-            if isinstance(options, list):
-                arg = options[int(arg)]
-            game = transition(game['session'], arg)
+            print
+            print get_message(id)
+            input = raw_input('> ')
+            transition(id, input)
         except:
             import traceback
             traceback.print_exc()
