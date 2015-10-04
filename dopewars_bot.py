7eff2faa5295c11122d49ee55ddaa384c956d46d
Pei-Lun Lee <ypcat6@gmail.com>
2015-10-04 14:13:40 +0000

    Trim bot message

--- dopewars_bot.py
+++ dopewars_bot.py
@@ -3,6 +3,7 @@
 import codecs
 import os
 import sys
+import traceback
 
 import telegram
 import dopewars
@@ -14,21 +15,20 @@ def main():
     bot = telegram.Bot(open(token_file).read().strip())
     update_id = 0
     while True:
-        for update in bot.getUpdates(offset=update_id, timeout=60):
-            try:
-                update_id = update.update_id + 1
+        try:
+            for update in bot.getUpdates(offset=update_id+1, timeout=60):
+                update_id = update.update_id
                 chat_id = update.message.chat_id
                 text = update.message.text.encode('utf8')
                 name = update.message.from_user.name
                 print name, text
-                messages = dopewars.play(name, text)
+                messages = dopewars.play(name, text.split('@')[0])
                 if messages:
                     text = '\n'.join(messages)
                     print text
                     bot.sendMessage(chat_id=chat_id, text=text)
-            except:
-                import traceback
-                traceback.print_exc()
+        except:
+            traceback.print_exc()
 
 if __name__ == '__main__':
     main()
