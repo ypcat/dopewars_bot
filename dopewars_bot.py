16db36fd045550cd6bc1d5df4e49e0ace9d8518b
Pei-Lun Lee <ypcat6@gmail.com>
2015-10-04 20:39:26 +0800

    Adapt bot to new api

--- dopewars_bot.py
+++ dopewars_bot.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python
 
 import telegram
-
-from dopewars import start, is_finish, get_message, process
+import dopewars
 
 def main():
-    token = open('token').read().strip()
-    bot = telegram.Bot(token)
+    bot = telegram.Bot(open('token').read().strip())
     update_id = 0
-    sessions = {}
     while True:
         for update in bot.getUpdates(offset=update_id, timeout=60):
             try:
                 update_id = update.update_id + 1
                 chat_id = update.message.chat_id
-                input = update.message.text.encode('utf8')
+                text = update.message.text.encode('utf8')
                 name = update.message.from_user.name
-                text = ''
-                print name, input
-                if input == 'start':
-                    id = start(name)
-                    sessions[chat_id] = id
-                    print "start new game, chat id %s, game id %d" % (chat_id, id)
-                    text = get_message(id)
-                elif chat_id in sessions and not is_finish(id):
-                    id = sessions[chat_id]
-                    if process(id, input):
-                        text = get_message(id)
-                if text:
+                print name, text
+                messages = dopewars.play(name, text)
+                if messages:
+                    text = '\n'.join(messages)
                     print text
                     bot.sendMessage(chat_id=chat_id, text=text)
             except:
@@ -36,4 +25,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
